e32d95960f61aa70e6ba299a93e0230f97b35661
mxlcompany <63812081+mxlcompany@users.noreply.github.com>
2021-02-20 22:40:55 +0200

    Создание собственной коллекции

---
+++ MyOwnCollection/MyOwnCollection/IHuman.cs
@@ -0,0 +1,17 @@
+﻿namespace MyOwnCollection
+{
+    /// <summary>
+    /// Человек.
+    /// </summary>
+    interface IHuman
+    {
+        /// <summary>
+        /// Имя человека.
+        /// </summary>
+        string Name { get; set; }
+        /// <summary>
+        /// Фамилия человека.
+        /// </summary>
+        string Surname { get; set; }
+    }
+}
